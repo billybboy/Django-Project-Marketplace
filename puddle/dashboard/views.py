85bdd81ad1c18e0f0e74ad23855490f5246487f6
billybboy <kk00915@live.com>
2023-05-12 00:24:02 +0800

    update README.md and fix minor bugs

--- puddle/dashboard/views.py
+++ puddle/dashboard/views.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 
-from item.models import Item
+from item.models import Item, Category
 
 @login_required
 def index(request):
     items = Item.objects.filter(created_by=request.user)
+    categories = Category.objects.all()
 
     return render(request, 'dashboard/index.html',{
         'items': items,
+        'categories': categories,
     })
 
 
